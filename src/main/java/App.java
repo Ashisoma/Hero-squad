60e1f03f145cd4bfb3a15ea2796a64f0889ce0ab
Ashisoma <okalionesmu@gmail.com>
2020-10-26 14:30:14 +0300

    add squad edit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,38 +18,39 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-    //static int getHerokuAssignedPort() {
-//        ProcessBuilder processBuilder = new ProcessBuilder();
-//        if (processBuilder.environment().get("PORT") != null) {
-//            return Integer.parseInt(processBuilder.environment().get("PORT"));
-//        }
-//        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-//    }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args) {
 
-        //port(getHerokuAssignedPort());
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
-
-        //String connectionString = "jdbc:postgresql://localhost:5432/hero"; //!
-        //Sql2o sql2o = new Sql2o(connectionString, "", ""); //!Connection con;
-        //Sql2o sql2o = new Sql2o(connectionString, "moringa","Access");
-        String connectionString = "jdbc:postgresql://ec2-18-211-86-133.compute-1.amazonaws.com:5432/d8seknpio0qb3o";
-        Sql2o sql2o = new Sql2o(connectionString,"anjjxtvbqbubbu","87a777b3175d76cb91b03c71764d8fbc5801514c6f9fa195979721e6703c0ae6");
+//ec2-18-211-86-133.compute-1.amazonaws.com:5432:d8seknpio0qb3o;
+        //anjjxtvbqbubbu //87a777b3175d76cb91b03c71764d8fbc5801514c6f9fa195979721e6703c0ae6
+        String connectionString = "jdbc:postgresql://localhost:5432/hero"; //!
+       //Sql2o sql2o = new Sql2o(connectionString, "", ""); //!Connection con;
+        Sql2o sql2o = new Sql2o(connectionString, "moringa", "Access");
+      //  String connectionString = "jdbc:postgresql://ec2-18-211-86-133.compute-1.amazonaws.com:5432/d8seknpio0qb3o";
+       // Sql2o sql2o = new Sql2o(connectionString,"anjjxtvbqbubbu","87a777b3175d76cb91b03c71764d8fbc5801514c6f9fa195979721e6703c0ae6");
         Sql2oSquadsDAO squadsDAO = new Sql2oSquadsDAO(sql2o);
         Sql2oHeroesDAO heroesDAO = new Sql2oHeroesDAO(sql2o);
         Map<String, Object> model = new HashMap<>();
 
-        get("/", (request, response) ->{
+        get("/", (request, response) -> {
             model.put("squads", squadsDAO.getAllSquads());
-            model.put("heroes",heroesDAO.getAllHeroes());
-            return new ModelAndView(model,"index.hbs");
+            model.put("heroes", heroesDAO.getAllHeroes());
+            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
 
-        get("/add-hero", (request, response) ->{
-          model.put("heroes",squadsDAO.getAllSquads());
-            return new ModelAndView(model,"hero-form.hbs");
+        get("/add-hero", (request, response) -> {
+            model.put("heroes", squadsDAO.getAllSquads());
+            return new ModelAndView(model, "hero-form.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/add-hero",(req,res)->{
@@ -65,16 +66,17 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 //
-        get("/add-squad",(request, response) -> {
+        get("/add-squad", (request, response) -> {
+            model.put("squads",squadsDAO.getAllSquads());
             return new ModelAndView(model, "squad.hbs");
         }, new HandlebarsTemplateEngine());
 
 
-        post("/add-squad", (req, res)->{
+        post("/add-squad", (req, res) -> {
             String name = req.queryParams("name");
             String cause = req.queryParams("cause");
             int size = Integer.parseInt(req.queryParams("size"));
-            Squad anotherSquad = new Squad(name, cause,size);
+            Squad anotherSquad = new Squad(name, cause, size);
             squadsDAO.addMySquad(anotherSquad);
             model.put("squads", squadsDAO.getAllSquads());
             return new ModelAndView(model, "index.hbs");
@@ -89,11 +91,11 @@ public class App {
             return new ModelAndView(model, "hero-details.hbs");
         }, new HandlebarsTemplateEngine());
 
-        get("/squads/:id",(req,res)->{
+        get("/squads/:id", (req, res) -> {
             int id = Integer.parseInt(req.params("id"));
-            model.put("heroes",squadsDAO.getHeroSquadsById(id));
+            model.put("heroes", squadsDAO.getHeroSquadsById(id));
             model.put("squads", squadsDAO.getSquadById(id));
-            return new ModelAndView(model,"squad-details.hbs");
+            return new ModelAndView(model, "squad-details.hbs");
         }, new HandlebarsTemplateEngine());
 
         get("/delete-hero/:id", (req, res) -> {
